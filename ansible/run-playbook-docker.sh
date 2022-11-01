ef1b689e68cf65dc386bd49cd5ec682a2f27fa0a
cptnalf <cptnalf@gmail.com>
2022-10-31 21:41:53 -0400

    docker-runner for ansible.

---
+++ ansible/run-playbook-docker.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+docker run --rm -it --name ansible-run \
+  -v $(pwd):/ansible \
+  -v ~/.ssh/id_ecdsa:/root/.ssh/id_ecdsa \
+  -v ~/.ssh/known_hosts:/root/.ssh/known_hosts \
+  -v ~/.ansible:/root/.ansible \
+  -v ${SSH_AUTH_SOCK}:/root/ssh-agent-file \
+  -e SSH_AUTH_SOCK=/root/ssh-agent-file \
+  --workdir=/ansible \
+  willhallonline/ansible:2.10-bullseye-slim \
+  ansible-playbook -u chiefengineer $@
+
+# -i prod_1.yml base.yml 
